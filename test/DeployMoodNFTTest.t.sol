593b60d46e0b15527e5dacd78d63307fc8e1b51f
George Gorzhiyev <gegorz@hotmail.com>
2025-01-29 21:32:20 -0700

    Added example svg to byte64 code

---
+++ test/DeployMoodNFTTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MoodNFT} from "src/MoodNFT.sol";
+
+contract DeployMoodNFT is Test {}
